92c4ddc5bef687f5828db4e9c395ff9c8071aaaa
lactose <yokotaniemi@gmail.com>
2023-09-01 20:31:39 -0700

    remove old bootloader definition

--- system/milk-machine/default.nix
+++ system/milk-machine/default.nix
@@ -29,11 +29,6 @@
     '';
   };
 
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.systemd-boot.configurationLimit = 20;
-
   # Enable powertop
   powerManagement.powertop.enable = true;
 
